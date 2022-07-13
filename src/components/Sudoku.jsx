546f1557e82d66c046ee059a3aa8f4ae99b3eb25
Lemmy Mwaura <lemminton@gmail.com>
2022-07-13 17:35:17 +0300

    refactor: sudoku component update

--- src/components/Sudoku.jsx
+++ src/components/Sudoku.jsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react"
-import { solveSudoku } from "./sudokuSolver"
-
-const puzzle = [
-  [3, 9, 0, 0, 5, 0, 0, 0, 0],
-  [0, 0, 0, 2, 0, 0, 0, 0, 5],
-  [0, 0, 0, 7, 1, 0, 0, 8, 0],
-  [0, 5, 0, 0, 6, 8, 0, 0, 0],
-  [2, 0, 6, 0, 0, 3, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 4],
-  [5, 0, 0, 0, 0, 0, 0, 0, 0],
-  [6, 7, 0, 1, 0, 5, 0, 4, 0],
-  [1, 0, 9, 0, 0, 0, 2, 0, 0],
-]
+import { useState } from "react"
+import { solveSudoku } from "./sudokuSolver";
+import image from '../assets/images/image.jpg';
 
 const Sudoku = () => {
+  const puzzle = [
+    [3, 9, 0, 0, 5, 0, 0, 0, 0],
+    [0, 0, 0, 2, 0, 0, 0, 0, 5],
+    [0, 0, 0, 7, 1, 0, 0, 8, 0],
+    [0, 5, 0, 0, 6, 8, 0, 0, 0],
+    [2, 0, 6, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0],
+    [6, 7, 0, 1, 0, 5, 0, 4, 0],
+    [1, 0, 9, 0, 0, 0, 2, 0, 0],
+  ]
+
   const [grid, setGrid] = useState(puzzle)
   const array = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-
   return (
     <div className="sudoku-grid">
-      <h4 className="title">sudoku visualiser</h4>
-      <div className="items">
+      <img src={image} alt='title-image'/>
+      <div className="wrapper">
         <div className="actions">
           <button
             className="btn-solve-grid"
@@ -40,9 +40,10 @@ const Sudoku = () => {
                     <td key={colIndex}>
                       <input
                         type="text"
-                        className="cell-input"
-                        value={grid[row][col] !== 0 ? grid[row][col] : ''}
-                        disabled={puzzle[row][col] !== 0}
+                        className={`cell-input ${
+                          puzzle[row][col] !== 0 && "original-cell"
+                        }`}
+                        value={grid[row][col] !== 0 ? grid[row][col] : ""}
                         readOnly
                       />
                     </td>
