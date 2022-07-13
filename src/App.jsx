210870c9449ed076010722d86035ea618d592bab
Lemmy Mwaura <lemminton@gmail.com>
2022-07-13 13:54:50 +0300

    refactor: renamed project name

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg'
-import './App.css'
+import "./App.css"
+import Sudoku from "./components/Sudoku"
 
 function App() {
   return (
     <div className="wrapper">
-   
+      <Sudoku />
     </div>
   )
 }
